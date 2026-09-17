1115f105bc1d09b7c66a15b64f12a7ca5835f1de
pbwsantu-collab <pbwsantu@gmail.com>
2026-09-17 20:08:38 +0530

    Fix Vite base path for renamed repo preposition-conjunction-

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/english-grammar-master/',
+  base: '/preposition-conjunction-/',
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#f8fafc',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/english-grammar-master/',
-        scope: '/english-grammar-master/',
+        start_url: '/preposition-conjunction-/',
+        scope: '/preposition-conjunction-/',
         icons: [
           {
             src: 'icons/icon-192.png',
